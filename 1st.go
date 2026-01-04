7e91a2816fecf60c74f15b3bf5d7775abea9e404
.env <chauhantushar6706@gmail.com>
2026-01-05 02:21:28 +0530

    variables done

--- 1st.go
+++ 1st.go
@@ -1,6 +1,6 @@
 package main
 import "fmt"
-
+import "unicode/utf8"
 
 func  main(){
 	var intNum int =  48535
@@ -8,4 +8,32 @@ func  main(){
 
 	var floatNum float = 345430.454
 	fmt.Println(floatNum)
+
+	var intNum1 int = 6 
+	var inNum2 int = 3
+	fmt.Println(intNum1/intNum2)
+	fmt.Println(intNum1%intNum2)
+
+
+	var myString string  = "Hello " + " " + "World"
+	fmt.Println(myString)
+
+	fmt.Println(len("Y")) // 2
+	fmt.Println(utf8.RuneCountInString("Y")) // 1
+
+	var myRune rune = 'a'
+	fmt.Println(myRune) // 97
+	
+
+	var myBoolean bool = true
+	fmt.Println(myBoolean) // true
+
+
+	var1 , var2 := 1,2
+	fmt.Println(var1,var2)
+
+	const  myConst string = "const value"
+	fmt.Println(myConst) 
+
+	
 }
